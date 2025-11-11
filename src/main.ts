9ac5e7cb4a09705236c273eb3b09d12ea87260be
Linda <lindakjellberg7@gmail.com>
2025-11-11 13:03:07 +0100

    Remove API key

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,7 @@ import { createHtml } from "./htmlUtils";
 import type { OmdbResponse } from "./models/OmdbResponse";
 import "./style.css";
 
-//#region Promise
-// const x = 100;
-
-// const p = new Promise((resolve: (value: boolean) => void, reject) => {
-//   setTimeout(() => {
-//     if (x >= 10) {
-//       resolve(true);
-//     } else {
-//       reject();
-//     }
-//   }, 4000);
-// });
-
-// p.then(
-//   (ok) => {
-//     console.log("Det fungerar:", ok);
-//   },
-//   () => {
-//     console.error("Det blev ett fel");
-//   }
-// );
-//#endregion
-
-// const f = fetch("https://omdbapi.com/?apikey=416ed51a&s=star");
-// f.then(() => {});
-
-fetch("https://omdbapi.com/?apikey=416ed51a&s=star")
+fetch("")
   .then((response) => response.json())
   .then((data: OmdbResponse) => {
     createHtml(data.Search);
