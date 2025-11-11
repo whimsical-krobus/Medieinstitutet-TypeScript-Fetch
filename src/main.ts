10fb9f9be5ce076bfef9fb2c3bbbe3cfae0e8439
Linda <lindakjellberg7@gmail.com>
2025-11-11 13:15:57 +0100

    Add API key as environmental variable

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createHtml } from "./htmlUtils";
 import type { OmdbResponse } from "./models/OmdbResponse";
 import "./style.css";
 
-fetch("")
+fetch(import.meta.env.VITE_API_URL)
   .then((response) => response.json())
   .then((data: OmdbResponse) => {
     createHtml(data.Search);
